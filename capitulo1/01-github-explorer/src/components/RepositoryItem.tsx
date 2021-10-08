d3a6e778e3245b51b16ce9afe81e06775605f8d3
Felipe Andrade <felipe.andrade@meza.digital>
2021-10-07 22:57:24 -0300

    Add github-explorer

---
+++ capitulo1/01-github-explorer/src/components/RepositoryItem.tsx
@@ -0,0 +1,22 @@
+interface RepositoryItemProps {
+  repository: {
+    name: string;
+    description: string;
+    html_url: string;
+  }
+}
+
+export function RepositoryItem (props: RepositoryItemProps) {
+  const {name, description, html_url} = props.repository
+
+  return (
+    <li>
+      <strong>{name}</strong>
+      <p>{description}</p>
+
+      <a href={html_url}>
+        Acessa Repositório
+      </a>
+    </li>
+  );
+}
